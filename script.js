892ef042da9e1d3cd3fbb52edfa8c9ceedb93e2a
Leonardo Gibertoni <gibertonileo@gmail.com>
2025-04-20 17:02:37 -0300

    att

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 function responderSim() {
-    // Exibe a mensagem "Mentirosa!!" ao clicar em "Sim"
     document.getElementById("mensagem").style.display = "block";
 }
 
 function moverBotao() {
     let botao = document.getElementById("nao");
-    let larguraTela = window.innerWidth - 120; // Para garantir que o botão não ultrapasse a tela
-    let alturaTela = window.innerHeight - 50; // Para garantir que o botão não ultrapasse a tela
+    
+    // Garante que o botão já está com posição absoluta
+    botao.style.position = "absolute";
 
-    let novaPosicaoX = Math.random() * larguraTela; // Gera uma nova posição X aleatória
-    let novaPosicaoY = Math.random() * alturaTela; // Gera uma nova posição Y aleatória
+    let larguraTela = window.innerWidth - botao.offsetWidth;
+    let alturaTela = window.innerHeight - botao.offsetHeight;
 
-    // Define a nova posição do botão "Não"
+    let novaPosicaoX = Math.random() * larguraTela;
+    let novaPosicaoY = Math.random() * alturaTela;
+
     botao.style.left = novaPosicaoX + "px";
     botao.style.top = novaPosicaoY + "px";
 }
